175f140825015a87d5130e2b6fe0c3851577ce88
Rohit <53703467+Rohit-Parameswaran@users.noreply.github.com>
2024-09-18 23:30:49 +0530

    Update 179. Largest Number.cpp

--- Leetcode/179. Largest Number.cpp
+++ Leetcode/179. Largest Number.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
-    static bool comparator(int& a, int& b) {
-        auto x = to_string(a), y = to_string(b);
-        return x + y > y + x;
-    };
-
     string largestNumber(vector<int>& nums) {
+        auto comparator = [] (int& a, int& b) {
+            auto x = to_string(a), y = to_string(b);
+            return x + y > y + x;
+        };
         sort(nums.begin(), nums.end(), comparator);
         string ans = "";
         for (auto it : nums)

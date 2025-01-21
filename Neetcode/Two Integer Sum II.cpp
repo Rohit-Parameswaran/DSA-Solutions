db5b138520d5c374e85d8eb3f9ce429334c487ec
Rohit <53703467+Rohit-Parameswaran@users.noreply.github.com>
2025-01-22 00:54:17 +0530

    Create Two Integer Sum II.cpp

---
+++ Neetcode/Two Integer Sum II.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n=numbers.size(), l=0, r=n-1;
+        while(l<r) {
+            if(numbers[l]+numbers[r] > target) --r;
+            else if(numbers[l]+numbers[r] < target) ++l;
+            else break;
+        }
+        return (vector<int>) {l+1,r+1};
+    }
+};

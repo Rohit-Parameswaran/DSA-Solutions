40d7b063398eb861ebcb86a8be1035eb75fc3546
Rohit <53703467+Rohit-Parameswaran@users.noreply.github.com>
2025-01-21 19:44:49 +0530

    Update Pow(x,n).cpp

--- Neetcode/Pow(x,n).cpp
+++ Neetcode/Pow(x,n).cpp
@@ -6,3 +6,24 @@ public:
         return n&1 ? x*myPow(x*x, n/2) : myPow(x*x, n/2);
     }
 };
+
+// Iterative
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == 0) return 1;
+
+        double ans = 1;
+        int power = abs((long)n);
+
+        while(power) {
+            if(power & 1){
+                ans *= x;
+            }
+            x *= x;
+            power /= 2;
+        }
+
+        return n<0 ? 1/ans : ans;
+    }
+};

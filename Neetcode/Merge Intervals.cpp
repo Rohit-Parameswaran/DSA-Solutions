37414a7316b44027d5f3e6faef8ab6a5788c7d82
Rohit <53703467+Rohit-Parameswaran@users.noreply.github.com>
2025-02-04 00:32:17 +0530

    Update Merge Intervals.cpp

--- Neetcode/Merge Intervals.cpp
+++ Neetcode/Merge Intervals.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(), [] (vector<int> a, vector<int> b) {
-            if(a[0] == b[0])
-                return a[1]<b[1];
-            return a[0] < b[0];
-        });
+        sort(intervals.begin(), intervals.end());
 
-        int n = intervals.size(), l=intervals[0][0], r=intervals[0][1], k=0;
-        vector<vector<int>> ans;
+        int n = intervals.size(), k=0;
+        vector<vector<int>> ans(1, intervals[0]);
 
-        ans.push_back(intervals[0]);
-
+        // ans.back() can be used instead of k
         for(int i=1; i<n ; ++i) {
             if(ans[k][1] < intervals[i][0]) {
                 ++k;

b5d6915a62ecd8a0e789a14ad9a7bf4d41364b25
Rohit <53703467+Rohit-Parameswaran@users.noreply.github.com>
2025-02-04 15:11:12 +0530

    Create Set Matrix Zeroes.cpp

---
+++ Neetcode/Set Matrix Zeroes.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int rowZero = 1;
+        for(int i=0; i<matrix.size(); ++i){
+            for(int j=0 ; j<matrix[0].size() ; ++j) {
+                if(matrix[i][j] == 0){
+                    matrix[0][j] = 0;
+                    if(i == 0)
+                        rowZero = 0;
+                    else
+                        matrix[i][0] = 0;
+                }
+            }
+        }
+
+        for(int i=1; i<matrix.size(); ++i)
+            for(int j=1 ; j<matrix[0].size() ; ++j)
+                if(matrix[0][j] == 0 || matrix[i][0] == 0)
+                        matrix[i][j] = 0;
+                
+        
+        if(matrix[0][0] == 0) {
+            for(int i=0; i<matrix.size() ; ++i)
+                matrix[i][0] = 0;
+        }
+        
+        if(rowZero == 0)
+            for(int i=0;i<matrix[0].size();++i)
+                matrix[0][i] = 0;
+    }
+};

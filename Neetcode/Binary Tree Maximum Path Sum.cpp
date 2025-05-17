ad1e836033575fcd25879688b056621a03cdae2a
Rohit <53703467+Rohit-Parameswaran@users.noreply.github.com>
2025-05-17 17:11:30 +0530

    Update Binary Tree Maximum Path Sum.cpp

--- Neetcode/Binary Tree Maximum Path Sum.cpp
+++ Neetcode/Binary Tree Maximum Path Sum.cpp
@@ -9,7 +9,38 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// Best Solution
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 
+class Solution {
+public:
+    int max_sum=INT_MIN;
+    int dfs(TreeNode* root, int &maxs) {
+        if(!root) return 0;
+        auto l = dfs(root->left, maxs);
+        auto r = dfs(root->right, maxs);
+        auto curs = max(root->val, max(l+root->val, r+root->val));
+        maxs = max(maxs, max(curs, root->val+l+r));
+        return curs;
+    }
+
+    int maxPathSum(TreeNode* root) {
+        dfs(root, max_sum);
+        return max_sum;
+    }
+};
+
+//My first solution
 struct PathInfo {
     int lsi;
     int rsi;

79305791f38a1b907569767d1b8ccb2ec5a1029a
Rohit <53703467+Rohit-Parameswaran@users.noreply.github.com>
2025-08-03 17:51:35 +0530

    Update Longest Increasing Subsequence.cpp

--- Neetcode/Longest Increasing Subsequence.cpp
+++ Neetcode/Longest Increasing Subsequence.cpp
@@ -1,3 +1,5 @@
+// O(n²)
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
@@ -11,5 +13,48 @@ public:
             dp[i] = maxLen;
         }
         return *max_element(dp.begin(), dp.end());
+    }
+};
+
+
+// O(nlogn)
+class Solution {
+public:
+    void insertIntoPile(vector<int> &tails, const int &num) {
+        auto it = lower_bound(tails.begin(), tails.end(), num);
+        if(it == tails.end()) tails.push_back(num);
+        else *it = num;
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> tails;
+        for(auto num: nums)
+            insertIntoPile(tails, num);
+        return tails.size();
+    }
+};
+
+// O(nlogn) -> My solution with manual binary search
+class Solution {
+public:
+    void insertIntoPile(vector<int> &tails, const int &num) {
+        int n=tails.size();
+        if(n == 0) tails.push_back(num);
+
+        int l=0, r=n-1, m;
+        while(l<r) {
+            m = l+(r-l)/2;
+            if(tails[m] >= num) r=m;
+            else l=m+1;
+        }
+        if(num > tails[l]) tails.push_back(num);
+        else tails[l] = min(tails[l], num);
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> tails;
+        for(auto num: nums)
+            insertIntoPile(tails, num);
+        return tails.size();
     }
 };

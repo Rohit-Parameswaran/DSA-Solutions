9207856b92d4153ca10269767a31843c340e06e4
Rohit <53703467+Rohit-Parameswaran@users.noreply.github.com>
2025-08-03 21:18:58 +0530

    Create Merge K Sorted Linked Lists.cpp

---
+++ Neetcode/Merge K Sorted Linked Lists.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<pair<int, ListNode*>> pq;
+        for(auto list: lists) pq.push({-list->val, list});
+        ListNode* head = new ListNode();
+        ListNode* tail = head;
+        while(!pq.empty()) {
+            auto curNode = (pq.top()).second;
+            pq.pop();
+            tail->next = curNode;
+            tail = tail->next;
+            if(curNode->next) pq.push({-(curNode->next)->val, curNode->next});
+        }
+        return head->next;
+    }
+};
